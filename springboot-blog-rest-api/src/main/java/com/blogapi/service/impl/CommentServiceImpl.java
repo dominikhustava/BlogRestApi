53bc8756bb7be477fd1a6c9acc5b13887cd0488a
dominikhustava <dominikhustava1@gmail.com>
2022-05-01 21:24:55 +0200

    Added third patry modelmapper library to map from model to dto

--- springboot-blog-rest-api/src/main/java/com/blogapi/service/impl/CommentServiceImpl.java
+++ springboot-blog-rest-api/src/main/java/com/blogapi/service/impl/CommentServiceImpl.java
@@ -8,6 +8,7 @@ import com.blogapi.payload.CommentDto;
 import com.blogapi.repository.CommentRepository;
 import com.blogapi.repository.PostRepository;
 import com.blogapi.service.CommentService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -20,9 +21,12 @@ public class CommentServiceImpl implements CommentService {
     private CommentRepository commentRepository;
     private PostRepository postRepository;
 
-    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository) {
+    private ModelMapper mapper;
+
+    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository, ModelMapper mapper) {
         this.commentRepository = commentRepository;
         this.postRepository = postRepository;
+        this.mapper = mapper;
     }
 
     @Override
@@ -104,21 +108,25 @@ public class CommentServiceImpl implements CommentService {
 
     private CommentDto mapToDTO(Comment comment){
 
-        CommentDto commentDto = new CommentDto();
+        CommentDto commentDto = mapper.map(comment, CommentDto.class);
+
+/*        CommentDto commentDto = new CommentDto();
         commentDto.setId(comment.getId());
         commentDto.setName(comment.getName());
         commentDto.setEmail(comment.getEmail());
-        commentDto.setBody(comment.getBody());
+        commentDto.setBody(comment.getBody());*/
         return  commentDto;
     }
 
     private Comment mapToEntity(CommentDto commentDto){
 
-        Comment comment = new Comment();
+        Comment comment = mapper.map(commentDto, Comment.class);
+
+/*        Comment comment = new Comment();
         comment.setId(commentDto.getId());
         comment.setName(commentDto.getName());
         comment.setEmail(commentDto.getEmail());
-        comment.setBody(commentDto.getBody());
+        comment.setBody(commentDto.getBody());*/
         return  comment;
     }
 
--- springboot-blog-rest-api/src/main/java/com/blogapi/service/impl/PostServiceImpl.java
+++ springboot-blog-rest-api/src/main/java/com/blogapi/service/impl/PostServiceImpl.java
@@ -6,6 +6,7 @@ import com.blogapi.payload.PostDto;
 import com.blogapi.payload.PostResponse;
 import com.blogapi.repository.PostRepository;
 import com.blogapi.service.PostService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,9 +22,11 @@ public class PostServiceImpl implements PostService {
 
     private PostRepository postRepository;
 
-    @Autowired
-    public PostServiceImpl(PostRepository postRepository) {
+    private ModelMapper mapper;
+
+    public PostServiceImpl(PostRepository postRepository, ModelMapper mapper) {
         this.postRepository = postRepository;
+        this.mapper = mapper;
     }
 
     @Override
@@ -91,20 +94,24 @@ public class PostServiceImpl implements PostService {
 
     // convert Entity into DTO
     private PostDto mapToDTO(Post post){
-        PostDto postDto = new PostDto();
-        postDto.setId(post.getId());
-        postDto.setTitle(post.getTitle());
-        postDto.setDescription(post.getDescription());
-        postDto.setContent(post.getContent());
+        PostDto postDto = mapper.map(post, PostDto.class);
+
+ //       PostDto postDto = new PostDto();
+ //       postDto.setId(post.getId());
+ //      postDto.setTitle(post.getTitle());
+ //       postDto.setDescription(post.getDescription());
+ //       postDto.setContent(post.getContent());
         return postDto;
     }
 
     // convert DTO to entity
     private Post mapToEntity(PostDto postDto){
-        Post post = new Post();
+        Post post = mapper.map(postDto, Post.class);
+
+/*        Post post = new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
-        post.setContent(postDto.getContent());
+        post.setContent(postDto.getContent());*/
         return post;
     }
 }
