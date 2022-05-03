5aa718b64569cabaaa53f0ff124f3f136c2d05e2
dominikhustava <dominikhustava1@gmail.com>
2022-05-03 20:34:58 +0200

    role based authentication on method level

--- springboot-blog-rest-api/src/main/java/com/blogapi/controller/PostController.java
+++ springboot-blog-rest-api/src/main/java/com/blogapi/controller/PostController.java
@@ -6,6 +6,7 @@ import com.blogapi.service.PostService;
 import com.blogapi.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -22,6 +23,7 @@ public class PostController {
         this.postService = postService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     // create blog post rest api
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
@@ -44,6 +46,7 @@ public class PostController {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     // update post by id rest api
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id){
@@ -53,6 +56,7 @@ public class PostController {
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     // delete post rest api
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id){
