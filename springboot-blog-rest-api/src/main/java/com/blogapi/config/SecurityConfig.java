2094ee6a1867a2820d3fc316de73db1c0aadc775
dominikhustava <dominikhustava1@gmail.com>
2022-05-10 00:13:07 +0200

    overriden configure(AuthenticationManagerBuilder auth) method for db authentication. Using previous custom implemented CustomUserDetailsService

--- springboot-blog-rest-api/src/main/java/com/blogapi/config/SecurityConfig.java
+++ springboot-blog-rest-api/src/main/java/com/blogapi/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.blogapi.config;
 
+import com.blogapi.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,6 +22,10 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 //to enable method level security
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -36,7 +43,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+    //db authentication
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+
+    //in memory authentication
+/*    @Override
     @Bean
     protected UserDetailsService userDetailsService() {
         UserDetails dominik = User.builder().username("dominik").password(passwordEncoder()
@@ -44,6 +60,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails admin = User.builder().username("admin").password(passwordEncoder()
                 .encode("admin")).roles("ADMIN").build();
         return new InMemoryUserDetailsManager(dominik, admin);
-    }
+    }*/
 
 }
